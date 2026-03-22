4de65a4454fcc5b6c9bdd6dbba1797495bd83865
Minerva Almeida Dias <minervaalmeida.dias@gmail.com>
2026-03-22 02:16:22 -0300

    Projeto Conta Bancaria finalizado

--- Menu.ts
+++ Menu.ts
@@ -1,47 +1,14 @@
-import readlinesync = require('readline-sync'); 
-import { colors } from './src/util/Colors'; 
+import { Conta } from "./src/util/model/Conta";
 
-export function main() {
-    let opcao: number;
+const c1 = new Conta(1, 123, 1, "Minerva Almeida", 1000);
+const c2 = new Conta(2, 456, 2, "João Silva", 500);
 
-    while (true) {
-        // Estilização conforme o enunciado (Item 78)
-        console.log(colors.bg.black, colors.fg.whitestrong, "*****************************************************");
-        console.log("                BANCO DO BRAZIL COM Z                ");
-        console.log("*****************************************************");
-        console.log("                                                     ");
-        console.log("            1 - Criar Conta                          ");
-        console.log("            2 - Listar todas as Contas               ");
-        console.log("            3 - Buscar Conta por Numero              ");
-        console.log("            4 - Atualizar Dados da Conta             ");
-        console.log("            5 - Apagar Conta                         ");
-        console.log("            6 - Sacar                                ");
-        console.log("            7 - Depositar                            ");
-        console.log("            8 - Transferir valores entre Contas      ");
-        console.log("            9 - Sair                                 ");
-        console.log("                                                     ");
-        console.log("*****************************************************");
-        console.log("                                                     ", colors.reset);
+c1.visualizar();
+c2.visualizar();
 
-        // Entrada de dados (Item 102)
-        opcao = readlinesync.questionInt("Entre com a opcao desejada: "); 
+c1.depositar(200);
+c1.sacar(100);
+c1.transferir(300, c2);
 
-        if (opcao === 9) {
-            console.log(colors.fg.greenstrong, "\nBanco do Brazil com Z - O seu Futuro começa aqui!\n", colors.reset);
-            process.exit(0);
-        }
-
-        switch (opcao) {
-            case 1: console.log("\nCriar Conta\n"); break;
-            case 2: console.log("\nListar todas as Contas\n"); break;
-            case 3: console.log("\nBuscar Conta por Numero\n"); break;
-            case 4: console.log("\nAtualizar Dados da Conta\n"); break;
-            case 5: console.log("\nApagar Conta\n"); break;
-            case 6: console.log("\nSacar\n"); break;
-            case 7: console.log("\nDepositar\n"); break;
-            case 8: console.log("\nTransferir valores entre Contas\n"); break;
-            default: console.log("\nOpção Inválida!\n"); break;
-        }
-    }
-}
-main();
+c1.visualizar();
+c2.visualizar();
